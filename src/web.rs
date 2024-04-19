fddb26b42aeb24cccfdd3369ee48bf54f72d22b6
Oleksii Karpenko <alexkarpenko@yahoo.com>
2024-04-19 22:47:23 +0300

    refactor web error handling

--- src/web.rs
+++ src/web.rs
@@ -4,8 +4,10 @@ use crate::{
     controllers::{State as AppState, TriggersState},
     WebhookTrigger,
 };
+use axum::http::HeaderMap;
+use axum::response::{IntoResponse, Response};
 use axum::{
-    extract::{Path, State},
+    extract::{FromRequest, Path, State},
     http::StatusCode,
     routing::get,
     Json, Router,
@@ -13,8 +15,11 @@ use axum::{
 use futures::Future;
 use kube::{Api, Client};
 use sacs::scheduler::{Scheduler, TaskScheduler};
+use sacs::task::TaskId;
+use serde::Serialize;
 use std::sync::Arc;
 use std::time::Duration;
+use strum::Display;
 use tokio::{
     net::TcpListener,
     sync::{watch, RwLock},
@@ -26,6 +31,42 @@ struct WebState {
     scheduler: Arc<RwLock<Scheduler>>,
     triggers: Arc<RwLock<TriggersState<WebhookTriggerSpec>>>,
     client: Client,
+}
+
+#[derive(FromRequest, Serialize)]
+#[from_request(via(axum::Json), rejection(AppError))]
+struct AppResponseJson {
+    status: AppResponseStatus,
+    message: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    task_id: Option<String>,
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "lowercase")]
+enum AppResponseStatus {
+    Ok,
+    Error,
+}
+
+#[derive(Display)]
+enum AppError {
+    #[strum(to_string = "multi-source hook isn't allowed")]
+    ForbiddenMultiSource,
+    #[strum(to_string = "trigger doesn't exist")]
+    TriggerNotFound,
+    #[strum(to_string = "source doesn't exist in the trigger")]
+    SourceNotFound,
+    #[strum(to_string = "authorization is required")]
+    Unauthorized,
+    #[strum(to_string = "forbidden")]
+    Forbidden,
+    #[strum(to_string = "KubeError: {msg}")]
+    KubeError { msg: String },
+    #[strum(to_string = "method isn't not implemented")]
+    NotImplemented,
+    #[strum(to_string = "TaskScheduler: {msg}")]
+    SchedulerError { msg: String },
 }
 
 pub async fn build_utils_web(
@@ -129,139 +170,163 @@ async fn handle_metrics(State(_state): State<AppState>) -> (StatusCode, String) 
 async fn handle_get_trigger_webhook(
     State(_state): State<WebState>,
     Path((namespace, trigger)): Path<(String, String)>,
-) -> (StatusCode, &'static str) {
+    _headers: HeaderMap,
+) -> Result<AppResponseJson, AppError> {
     warn!("webhook: get trigger hook isn't implemented");
-    warn!("webhook: GET, namespace={namespace}, trigger={trigger}");
-    (StatusCode::NOT_IMPLEMENTED, "Not implemented")
+    debug!("webhook: GET, namespace={namespace}, trigger={trigger}");
+    Err(AppError::NotImplemented)
 }
 
 async fn handle_post_trigger_webhook(
     State(state): State<WebState>,
     Path((namespace, trigger)): Path<(String, String)>,
-) -> (StatusCode, Json<serde_json::Value>) {
+    _headers: HeaderMap,
+) -> Result<AppResponseJson, AppError> {
     debug!("webhook: POST, namespace={namespace}, trigger={trigger}");
 
     let triggers_api: Api<WebhookTrigger> = Api::namespaced(state.client.clone(), &namespace);
-    match triggers_api.get(&trigger).await {
-        Ok(trigger) => {
-            let trigger_hash_key = trigger.trigger_hash_key();
-            if trigger.spec.webhook.multi_source {
-                info!("Run all sources task for trigger {trigger_hash_key}");
-                let task = trigger.create_trigger_task(
-                    state.client.clone(),
-                    sacs::task::TaskSchedule::Once,
-                    None,
-                    state.triggers.clone(),
-                );
-                let scheduler = state.scheduler.write().await;
-                let task_id = scheduler.add(task).await.unwrap(); // TODO: get rid of unwrap
+    let trigger = triggers_api.get(&trigger).await?;
+    let trigger_hash_key = trigger.trigger_hash_key();
 
-                (
-                    StatusCode::ACCEPTED,
-                    Json(
-                        serde_json::json!({"status": "ok", "message": "Accepted", "task_id": task_id.to_string()}),
-                    ),
-                )
-            } else {
-                warn!("try to run multi-source hook on trigger {trigger_hash_key}");
-                (
-                    StatusCode::BAD_REQUEST,
-                    Json(
-                        serde_json::json!({"status": "error", "message": "Multi-source hooks isn't allowed"}),
-                    ),
-                )
-            }
-        }
-        Err(err) => match err {
-            kube::Error::Api(err) => {
-                if err.code == 404 {
-                    (
-                        StatusCode::NOT_FOUND,
-                        Json(
-                            serde_json::json!({"status": "error", "message": "requested trigger doesn't exist"}),
-                        ),
-                    )
-                } else {
-                    (
-                        StatusCode::INTERNAL_SERVER_ERROR,
-                        Json(serde_json::json!({"status": "error", "message": err})),
-                    )
-                }
-            }
-            _ => (
-                StatusCode::INTERNAL_SERVER_ERROR,
-                Json(serde_json::json!({"status": "error", "message": err.to_string()})),
-            ),
-        },
+    if trigger.spec.webhook.multi_source {
+        info!("Run all sources task for trigger {trigger_hash_key}");
+        let task = trigger.create_trigger_task(
+            state.client.clone(),
+            sacs::task::TaskSchedule::Once,
+            None,
+            state.triggers.clone(),
+        );
+        let scheduler = state.scheduler.write().await;
+        let task_id = scheduler.add(task).await?;
+
+        Ok(task_id.into())
+    } else {
+        warn!("try to run forbidden multi-source hook on trigger {trigger_hash_key}");
+        Err(AppError::ForbiddenMultiSource)
     }
 }
 
 async fn handle_get_source_webhook(
     State(_state): State<WebState>,
     Path((namespace, trigger, source)): Path<(String, String, String)>,
-) -> (StatusCode, &'static str) {
+    _headers: HeaderMap,
+) -> Result<AppResponseJson, AppError> {
     warn!("webhook: get source hook isn't implemented");
-    warn!("webhook: GET, namespace={namespace}, trigger={trigger}, source={source}");
-    (StatusCode::NOT_IMPLEMENTED, "Not implemented")
+    debug!("webhook: GET, namespace={namespace}, trigger={trigger}, source={source}");
+    Err(AppError::NotImplemented)
 }
 
 async fn handle_post_source_webhook(
     State(state): State<WebState>,
     Path((namespace, trigger, source)): Path<(String, String, String)>,
-) -> (StatusCode, Json<serde_json::Value>) {
+    _headers: HeaderMap,
+) -> Result<AppResponseJson, AppError> {
     debug!("webhook: POST, namespace={namespace}, trigger={trigger}, source={source}");
 
     let triggers_api: Api<WebhookTrigger> = Api::namespaced(state.client.clone(), &namespace);
-    match triggers_api.get(&trigger).await {
-        Ok(trigger) => {
-            let trigger_hash_key = trigger.trigger_hash_key();
-            if trigger.spec.sources.names.contains(&source) {
-                info!("Run source task {trigger_hash_key}/{source}");
-                let task = trigger.create_trigger_task(
-                    state.client.clone(),
-                    sacs::task::TaskSchedule::Once,
-                    Some(source.clone()),
-                    state.triggers.clone(),
-                );
-                let scheduler = state.scheduler.write().await;
-                let task_id = scheduler.add(task).await.unwrap(); // TODO: get rid of unwrap
+    let trigger = triggers_api.get(&trigger).await?;
+    let trigger_hash_key = trigger.trigger_hash_key();
 
-                (
-                    StatusCode::ACCEPTED,
-                    Json(
-                        serde_json::json!({"status": "ok", "message": "Accepted", "task_id": task_id.to_string()}),
-                    ),
-                )
-            } else {
-                warn!("source `{source}` doesn't exist in trigger {trigger_hash_key}");
-                (
-                    StatusCode::BAD_REQUEST,
-                    Json(
-                        serde_json::json!({"status": "error", "message": "requested source doesn't exist in the trigger"}),
-                    ),
-                )
-            }
+    if trigger.spec.sources.names.contains(&source) {
+        info!("Run source task {trigger_hash_key}/{source}");
+        let task = trigger.create_trigger_task(
+            state.client.clone(),
+            sacs::task::TaskSchedule::Once,
+            Some(source.clone()),
+            state.triggers.clone(),
+        );
+        let scheduler = state.scheduler.write().await;
+        let task_id = scheduler.add(task).await?;
+
+        Ok(task_id.into())
+    } else {
+        warn!("source `{source}` doesn't exist in trigger {trigger_hash_key}");
+        Err(AppError::SourceNotFound)
+    }
+}
+
+impl IntoResponse for AppResponseJson {
+    fn into_response(self) -> Response {
+        (StatusCode::ACCEPTED, Json(self)).into_response()
+    }
+}
+
+impl From<TaskId> for AppResponseJson {
+    fn from(value: TaskId) -> Self {
+        Self {
+            status: AppResponseStatus::Ok,
+            message: "job has been scheduled".into(),
+            task_id: Some(value.to_string()),
         }
-        Err(err) => match err {
+    }
+}
+
+impl From<String> for AppResponseJson {
+    fn from(value: String) -> Self {
+        Self {
+            status: AppResponseStatus::Ok,
+            message: value,
+            task_id: None,
+        }
+    }
+}
+
+impl From<&str> for AppResponseJson {
+    fn from(value: &str) -> Self {
+        Self {
+            status: AppResponseStatus::Ok,
+            message: value.to_string(),
+            task_id: None,
+        }
+    }
+}
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        let resp = AppResponseJson {
+            status: AppResponseStatus::Error,
+            message: self.to_string(),
+            task_id: None,
+        };
+
+        let status: StatusCode = match self {
+            AppError::ForbiddenMultiSource => StatusCode::BAD_REQUEST,
+            AppError::TriggerNotFound => StatusCode::NOT_FOUND,
+            AppError::SourceNotFound => StatusCode::NOT_FOUND,
+            AppError::Unauthorized => StatusCode::UNAUTHORIZED,
+            AppError::Forbidden => StatusCode::FORBIDDEN,
+            AppError::KubeError { msg: _ } => StatusCode::INTERNAL_SERVER_ERROR,
+            AppError::SchedulerError { msg: _ } => StatusCode::INTERNAL_SERVER_ERROR,
+            AppError::NotImplemented => StatusCode::NOT_IMPLEMENTED,
+        };
+
+        (status, resp).into_response()
+    }
+}
+
+impl From<kube::Error> for AppError {
+    fn from(value: kube::Error) -> Self {
+        match &value {
             kube::Error::Api(err) => {
                 if err.code == 404 {
-                    (
-                        StatusCode::NOT_FOUND,
-                        Json(
-                            serde_json::json!({"status": "error", "message": "requested trigger doesn't exist"}),
-                        ),
-                    )
+                    Self::TriggerNotFound
                 } else {
-                    (
-                        StatusCode::INTERNAL_SERVER_ERROR,
-                        Json(serde_json::json!({"status": "error", "message": err})),
-                    )
+                    Self::KubeError {
+                        msg: err.to_string(),
+                    }
                 }
             }
-            _ => (
-                StatusCode::INTERNAL_SERVER_ERROR,
-                Json(serde_json::json!({"status": "error", "message": err.to_string()})),
-            ),
-        },
+            _ => Self::KubeError {
+                msg: value.to_string(),
+            },
+        }
+    }
+}
+
+impl From<sacs::Error> for AppError {
+    fn from(value: sacs::Error) -> Self {
+        Self::SchedulerError {
+            msg: value.to_string(),
+        }
     }
 }
