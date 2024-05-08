b2ce2a0e85be595638c46c9e65dadf86ed5306b6
Oleksii Karpenko <alexkarpenko@yahoo.com>
2024-05-08 22:08:08 +0300

    bump version [skip ci]

--- charts/prepare-crds.sh
+++ charts/prepare-crds.sh
@@ -5,12 +5,6 @@ BASE_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 CHART_DIR="${BASE_DIR}/git-events-runner"
 BUNDLE_FILE=${BASE_DIR}/crds.yaml
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  SED="gsed"
-else
-  SED="sed"
-fi
-
 # generate CRD bundle from the latest binary
 APP_VERSION=${LOCAL_APP_VERSION:-$(yq .appVersion ${CHART_DIR}/Chart.yaml)}
 APP_IMAGE=${LOCAL_APP_IMAGE:-$(yq .image.repository ${CHART_DIR}/values.yaml)}
