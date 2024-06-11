fa86353aedffd9c7697a26a9fc175d1dbe9f6def
Oleksii Karpenko <alexkarpenko@yahoo.com>
2024-06-11 17:08:49 +0200

    Removed everything with mike 2.1.1

--- v0.1/examples/upgrade-helm-release.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-helm repo add alex-karpenko https://alex-karpenko.github.io/helm-charts
-helm repo update
-
-helm upgrade git-events-runner alex-karpenko/git-events-runner --namespace git-events-runner
